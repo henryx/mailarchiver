50f838579179af96ae9346554cc7e893d91b9e82
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-07 16:11:46 +0100

    Started project

---
+++ src/main/java/com/application/mailarchive/Main.java
@@ -0,0 +1,19 @@
+/*
+ Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       mailarchive
+ Description   A mail archive
+ License       GPL version 2 (see LICENSE for details)
+ */
+package com.application.mailarchive;
+
+/**
+ *
+ * @author Enrico Bianchi <enrico.bianchi@gmail.com>
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
