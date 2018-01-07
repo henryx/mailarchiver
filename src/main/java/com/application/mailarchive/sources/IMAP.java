ca0cf52dd7e0734ae10f32d7739f26a3d10d9bbb
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-07 16:46:38 +0100

    Added class for managing IMAP connections

---
+++ src/main/java/com/application/mailarchive/sources/IMAP.java
@@ -0,0 +1,48 @@
+/*
+ Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       mailarchive
+ Description   A mail archiver
+ License       GPL version 2 (see LICENSE for details)
+ */
+package com.application.mailarchive.sources;
+
+import com.application.mailarchive.exceptions.UnsupportedProtocolException;
+
+/**
+ *
+ * @author Enrico Bianchi <enrico.bianchi@gmail.com>
+ */
+public class IMAP extends Protocol {
+
+    public IMAP() {
+        super();
+
+        this.setPort(143);
+    }
+
+    public IMAP(String host, String user, String password, String protocol) throws UnsupportedProtocolException {
+        super(host, user, password, protocol);
+
+        if (!protocol.equals("imap") || protocol.equals("imaps")) {
+            throw new UnsupportedProtocolException(protocol);
+        }
+    }
+
+    public IMAP(String host, int port, String user, String password, String protocol) throws UnsupportedProtocolException {
+        super(host, port, user, password, protocol);
+
+        if (!protocol.equals("imap") || protocol.equals("imaps")) {
+            throw new UnsupportedProtocolException(protocol);
+        }
+    }
+
+    @Override
+    public void open() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
