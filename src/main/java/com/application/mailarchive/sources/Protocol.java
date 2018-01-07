25a10f8280f06dd25988e14c1a8412f8c9a199ec
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-07 16:46:08 +0100

    Added abstract class for managing protocols

---
+++ src/main/java/com/application/mailarchive/sources/Protocol.java
@@ -0,0 +1,113 @@
+/*
+ Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       mailarchive
+ Description   A mail archiver
+ License       GPL version 2 (see LICENSE for details)
+ */
+package com.application.mailarchive.sources;
+
+import com.application.mailarchive.exceptions.UnsupportedProtocolException;
+
+/**
+ *
+ * @author Enrico Bianchi <enrico.bianchi@gmail.com>
+ */
+public abstract class Protocol implements AutoCloseable {
+
+    private int port;
+    private String host;
+    private String user;
+    private String password;
+    private String protocol;
+
+    public Protocol() {
+    }
+
+    public Protocol(String host, String user, String password, String protocol) throws UnsupportedProtocolException {
+        this.host = host;
+        this.user = user;
+        this.password = password;
+    }
+
+    public Protocol(String host, int port, String user, String password, String protocol) throws UnsupportedProtocolException {
+        this.host = host;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+    }
+
+    /**
+     * @return the port
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * @param port the port to set
+     */
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    /**
+     * @return the host
+     */
+    public String getHost() {
+        return host;
+    }
+
+    /**
+     * @param host the host to set
+     */
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    /**
+     * @param protocol the password to set
+     */
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+    
+    /**
+     * @return the protocol
+     */
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public abstract void open();
+
+    @Override
+    public abstract void close();
+}
