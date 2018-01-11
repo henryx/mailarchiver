b774865da31ebc3ef7c4e6a82b0c3b57959c04fb
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-12 00:06:07 +0100

    Added class

---
+++ src/main/java/com/application/mailarchive/operations/Archive.java
@@ -0,0 +1,20 @@
+/*
+ Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       mailarchive
+ Description   A mail archiver
+ License       GPL version 2 (see LICENSE for details)
+ */
+package com.application.mailarchive.operations;
+
+import org.ini4j.Wini;
+
+/**
+ *
+ * @author Enrico Bianchi <enrico.bianchi@gmail.com>
+ */
+public class Archive {
+    
+    public void execute(Wini cfg) {
+        
+    }
+}
