e23b1c36fc22b2e59f21d35d1ac75bcb2735890b
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-15 15:21:11 +0100

    Started IMAP archive

--- src/main/java/com/application/mailarchive/operations/Archive.java
+++ src/main/java/com/application/mailarchive/operations/Archive.java
@@ -6,6 +6,12 @@
  */
 package com.application.mailarchive.operations;
 
+import com.application.mailarchive.exceptions.UnsupportedProtocolException;
+import com.application.mailarchive.sources.IMAP;
+import com.application.mailarchive.sources.Protocol;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import org.ini4j.Profile.Section;
 import org.ini4j.Wini;
 
 /**
@@ -13,8 +19,28 @@ import org.ini4j.Wini;
  * @author Enrico Bianchi <enrico.bianchi@gmail.com>
  */
 public class Archive {
-    
-    public void execute(Wini cfg) {
-        
+
+    private void startIMAP(Section section) throws UnsupportedProtocolException, NoSuchProviderException, MessagingException {
+        try (Protocol proto = new IMAP(section.get("protocol"));) {
+
+            proto.setUser(section.get("user"));
+            proto.setPassword(section.get("password"));
+            proto.setHost(section.get("host"));
+            proto.setPort(Integer.parseInt(section.get(section, "port")));
+
+            proto.connect();
+            // TODO: fetch and save messages
+        }
+    }
+
+    public void execute(Wini cfg) throws NoSuchProviderException, UnsupportedProtocolException, MessagingException {
+
+        for (String section : cfg.keySet()) {
+            if (!(section.equals("general") || section.equals("logging"))) {
+                if (cfg.get(section, "protocol").startsWith("imap")) {
+                    this.startIMAP(cfg.get(section));
+                }
+            }
+        }
     }
 }
