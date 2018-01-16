5603254b63a64ab8d6ce628936a8244d9e2f4670
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-16 23:53:11 +0100

    Implemented abstract methods

--- src/main/java/com/application/mailarchive/store/database/SQLiteDB.java
+++ src/main/java/com/application/mailarchive/store/database/SQLiteDB.java
@@ -7,6 +7,7 @@
 package com.application.mailarchive.store.database;
 
 import com.application.mailarchive.store.Database;
+import java.sql.Connection;
 import org.ini4j.Wini;
 
 /**
@@ -14,8 +15,13 @@ import org.ini4j.Wini;
  * @author Enrico Bianchi <enrico.bianchi@gmail.com>
  */
 public class SQLiteDB extends Database {
-    
+
     public SQLiteDB(Wini cfg) {
         super(cfg);
+    }
+
+    @Override
+    public Connection open() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
