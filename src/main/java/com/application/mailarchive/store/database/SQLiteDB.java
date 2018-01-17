beb740e382c8bd94931d3861171e6c4f63f2d2bb
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-17 17:02:18 +0100

    Removed check if file exists

--- src/main/java/com/application/mailarchive/store/database/SQLiteDB.java
+++ src/main/java/com/application/mailarchive/store/database/SQLiteDB.java
@@ -7,8 +7,6 @@
 package com.application.mailarchive.store.database;
 
 import com.application.mailarchive.store.Database;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -26,25 +24,18 @@ public class SQLiteDB extends Database {
         super(cfg);
     }
 
-    private void initDB(Boolean init) {
+    private void initDB() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void open() throws SQLException {
-        Boolean init;
         String dbpath;
 
         dbpath = this.cfg.get("general", "database");
 
-        if (Files.exists(Paths.get(dbpath))) {
-            init = Boolean.TRUE;
-        } else {
-            init = Boolean.FALSE;
-        }
-
         this.conn = DriverManager.getConnection("jdbc:sqlite:" + dbpath);
-        this.initDB(init);
+        this.initDB();
     }
 
     @Override
