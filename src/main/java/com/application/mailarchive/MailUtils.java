b52f2f07c50eb5b7adb9df4b78a5e01c3b49eefa
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-03-23 00:49:08 +0100

    Created utils class

---
+++ src/main/java/com/application/mailarchive/MailUtils.java
@@ -0,0 +1,14 @@
+/*
+ Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       mailarchive
+ Description   A mail archiver
+ License       GPL version 2 (see LICENSE for details)
+ */
+package com.application.mailarchive;
+
+/**
+ * @author Enrico Bianchi <enrico.bianchi@gmail.com>
+ */
+public class MailUtils {
+    
+}
