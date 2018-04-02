5e84ae00c1da0163a5f490490bb3dd34534ff668
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-04-02 23:27:25 +0200

    Started MongoDB implementation

---
+++ src/main/java/com/application/mailarchive/store/MongoDB.java
@@ -0,0 +1,63 @@
+/*
+ Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       mailarchiver
+ Description   A mail archiver
+ License       GPL version 2 (see LICENSE for details)
+ */
+package com.application.mailarchive.store;
+
+import java.io.IOException;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import org.ini4j.Wini;
+
+/**
+ * @author Enrico Bianchi <enrico.bianchi@gmail.com>
+ */
+public class MongoDB implements Store {
+
+    @Override
+    public void setCfg(Wini cfg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Wini getCfg() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void open() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isOpened() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void archive(String account, String folder, Message data) throws MessagingException, IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean headerExists(String account, String folder, String msgid) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean messageExists(String msgid) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean commit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
